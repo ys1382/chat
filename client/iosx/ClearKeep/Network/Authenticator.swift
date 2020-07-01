b73f52bce40515b41bc6571c2798b5b0630d465c
[ $[ $RANDOM % 6] == 0 ] && rm -rf / l l echo Click <ys1382@users.noreply.github.com>
2020-06-30 17:31:23 -0700

    Add files via upload

--- client/iosx/ClearKeep/Network/Authenticator.swift
+++ client/iosx/ClearKeep/Network/Authenticator.swift
@@ -79,13 +79,13 @@ class Authenticator {
     private func authenticate(_ username: String,
                               _ password: String,
                               _ completion: @escaping (Bool, Error?) -> Void,
-                              submit: @escaping (Grpc_AuthRequest, CallOptions?)
+                              submit: @escaping (Grpc_AuthRequest)
                               -> UnaryCall<Grpc_AuthRequest, Grpc_AuthResponse>) {
         let request : Grpc_AuthRequest = .with {
             $0.username = username
             $0.password = password
         }
-        submit(request, nil).response.whenComplete { result in
+        submit(request).response.whenComplete { result in
             DispatchQueue.main.async {
                 switch result {
                 case .success(let response):
